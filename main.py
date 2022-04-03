953c550c85c0c781bd9dd4441a26340126313674
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-04-03 19:54:05 +0530

    M

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -10,7 +11,19 @@ def index():
 def get_all_blogs():
     return {'message' : 'All blogs provided'}
 
-    
+
+class BlogType(str , Enum):
+    # Defining blog types
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type : BlogType):
+    return {'message' : f'Blog type {type}'}
+
+
+
 @app.get('/blog/{id}')
 def get_blog(id : int) :
     return {'message' : f'Blog with id {id}'}
